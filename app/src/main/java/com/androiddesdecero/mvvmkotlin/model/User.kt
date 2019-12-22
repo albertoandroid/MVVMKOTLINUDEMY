383b72eb4673b1a13e7218ab7eedcba07254d2c3
Alberto <info@androiddesdecero.com>
2019-12-22 17:03:50 +0100

    2

---
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/model/User.kt
@@ -0,0 +1,20 @@
+package com.androiddesdecero.mvvmkotlin.model
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+
+    @field:SerializedName("login")
+    val login: String,
+    @field:SerializedName("avatar_url")
+    val avatarUrl: String?,
+    @field:SerializedName("name")
+    val name: String?,
+    @field:SerializedName("company")
+    val company: String?,
+    @field:SerializedName("repos_url")
+    val reposUrl: String?,
+    @field:SerializedName("blog")
+    val blog: String?
+
+)
