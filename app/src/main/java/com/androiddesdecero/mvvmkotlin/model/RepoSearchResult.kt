6c37ae1389446c727e8e3bd660ca065fd8e04142
Alberto <info@androiddesdecero.com>
2019-12-22 19:12:08 +0100

    5

---
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/model/RepoSearchResult.kt
@@ -0,0 +1,14 @@
+package com.androiddesdecero.mvvmkotlin.model
+
+import androidx.room.Entity
+import androidx.room.TypeConverters
+import com.androiddesdecero.mvvmkotlin.db.GithubTypeConverters
+
+@Entity(primaryKeys = ["query"])
+@TypeConverters(GithubTypeConverters::class)
+class RepoSearchResult (
+    val query: String,
+    val reporIds: List<Int>,
+    val totalCount: Int,
+    val next: Int?
+)
