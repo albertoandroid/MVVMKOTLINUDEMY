f4ec3d2e7572cd6a8a30ec83f70341d73210bcdc
Alberto <info@androiddesdecero.com>
2019-12-22 19:23:01 +0100

    3

---
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/db/UserDao.kt
@@ -0,0 +1,17 @@
+package com.androiddesdecero.mvvmkotlin.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.androiddesdecero.mvvmkotlin.model.User
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(user: User)
+
+    @Query("SELECT * FROM user WHERE login = :login")
+    fun findByLogin(login: String): LiveData<User>
+}
