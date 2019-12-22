eb9d9479e5e52fec424dc77f5e0920f7c094c603
Alberto <info@androiddesdecero.com>
2019-12-22 19:06:21 +0100

    3

---
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/db/GithubTypeConverters.kt
@@ -0,0 +1,28 @@
+package com.androiddesdecero.mvvmkotlin.db
+
+import android.util.Log
+import androidx.room.TypeConverter
+import java.lang.NumberFormatException
+
+object GithubTypeConverters {
+    @TypeConverter
+    @JvmStatic
+    fun stringToIntList(data: String?): List<Int>?{
+        return data?.let{
+            it.split(",").map{
+                try{
+                    it.toInt()
+                } catch (ex: NumberFormatException){
+                    Log.d("TAG1", "No puede convertir a numero")
+                    null
+                }
+            }?.filterNotNull()
+        }
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun intListToString(ints: List<Int>?): String? {
+        return ints?.joinToString { "," }
+    }
+}
