081691d5df321de3b7429ac6ead92ad2c661cabe
Alberto <info@androiddesdecero.com>
2019-12-23 13:01:54 +0100

    2

--- app/src/main/java/com/androiddesdecero/mvvmkotlin/repository/NetworkBoundResource.kt
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/repository/NetworkBoundResource.kt
@@ -1,9 +1,12 @@
 package com.androiddesdecero.mvvmkotlin.repository
 
 import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import com.androiddesdecero.mvvmkotlin.AppExecutors
+import com.androiddesdecero.mvvmkotlin.api.ApiResponse
+import com.androiddesdecero.mvvmkotlin.api.ApiSuccessResponse
 
 abstract class NetworkBoundResource<ResultType, RequestType>
 @MainThread constructor(private val appExecutors: AppExecutors){
@@ -16,7 +19,18 @@ abstract class NetworkBoundResource<ResultType, RequestType>
             result.removeSource(dbSource)
             if(shouldFetch(data)){
                 fetchFromNetwork(dbSource)
+            } else{
+                result.addSource(dbSource){newData->
+                    setValue(Resource.success(newData))
+                }
             }
+        }
+    }
+
+    @MainThread
+    private fun setValue(newValue: Resource<ResultType>){
+        if(result.value != newValue){
+            result.value = newValue
         }
     }
 
@@ -24,9 +38,22 @@ abstract class NetworkBoundResource<ResultType, RequestType>
 
     }
 
+    protected open fun onFetchFailed(){}
+
+    fun asLiveData() = result as LiveData<Resource<ResultType>>
+
+    @WorkerThread
+    protected open fun processResponse(response: ApiSuccessResponse<RequestType>) = response.body
+
+    @WorkerThread
+    protected abstract fun saveCallResult(item: RequestType)
+
     @MainThread
     protected abstract fun shouldFetch(data: ResultType?): Boolean
 
     @MainThread
     protected abstract fun loadFromDb(): LiveData<ResultType>
+
+    @MainThread
+    protected abstract fun createCall(): LiveData<ApiResponse<RequestType>>
 }
