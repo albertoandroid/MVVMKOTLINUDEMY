3d2873c51046e7621cd284d1bfb9b1867f662f55
Alberto <info@androiddesdecero.com>
2019-12-23 12:50:15 +0100

    3

---
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/repository/NetworkBoundResource.kt
@@ -0,0 +1,32 @@
+package com.androiddesdecero.mvvmkotlin.repository
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.androiddesdecero.mvvmkotlin.AppExecutors
+
+abstract class NetworkBoundResource<ResultType, RequestType>
+@MainThread constructor(private val appExecutors: AppExecutors){
+    private val result = MediatorLiveData<Resource<ResultType>>()
+
+    init {
+        result.value = Resource.loading(null)
+        val dbSource = loadFromDb()
+        result.addSource(dbSource){data->
+            result.removeSource(dbSource)
+            if(shouldFetch(data)){
+                fetchFromNetwork(dbSource)
+            }
+        }
+    }
+
+    private fun fetchFromNetwork(dbSource: LiveData<ResultType>){
+        
+    }
+
+    @MainThread
+    protected abstract fun shouldFetch(data: ResultType?): Boolean
+
+    @MainThread
+    protected abstract fun loadFromDb(): LiveData<ResultType>
+}
