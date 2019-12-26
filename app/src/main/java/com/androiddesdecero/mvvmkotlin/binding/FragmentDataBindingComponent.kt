a5bc8f5d2f02941fb1b1074af2fa5b5a1c9b84f9
Alberto <info@androiddesdecero.com>
2019-12-26 13:40:29 +0100

    3

---
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/binding/FragmentDataBindingComponent.kt
@@ -0,0 +1,11 @@
+package com.androiddesdecero.mvvmkotlin.binding
+
+import androidx.databinding.DataBindingComponent
+import androidx.fragment.app.Fragment
+
+class FragmentDataBindingComponent(fragment: Fragment): DataBindingComponent {
+
+    private val adapter: FragmentBindingAdapters(fragment)
+
+    override fun getFragmentBindingAdapters(): FragmentBindingAdapters = adapter
+}
