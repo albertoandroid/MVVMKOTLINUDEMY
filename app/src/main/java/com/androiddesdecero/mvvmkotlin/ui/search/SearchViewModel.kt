b31a89eea89fa131da9b86fcd5700f2f5fe47cfc
Alberto <info@androiddesdecero.com>
2019-12-26 16:16:59 +0100

    3

--- app/src/main/java/com/androiddesdecero/mvvmkotlin/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/ui/search/SearchViewModel.kt
@@ -1,7 +1,13 @@
 package com.androiddesdecero.mvvmkotlin.ui.search
 
+import android.app.DownloadManager
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.androiddesdecero.mvvmkotlin.repository.RepoRepository
+import com.androiddesdecero.mvvmkotlin.repository.Resource
+import com.androiddesdecero.mvvmkotlin.repository.Status
 import javax.inject.Inject
 
 class SearchViewModel @Inject constructor(repoRepository: RepoRepository): ViewModel() {
@@ -17,6 +23,83 @@ class SearchViewModel @Inject constructor(repoRepository: RepoRepository): ViewM
             }
             handleError = true
             return errorMessage
+        }
+    }
+
+    class NextPageHandler(private val repository: RepoRepository): Observer<Resource<Boolean>>{
+
+        private var nextPageLiveData: LiveData<Resource<Boolean>>? = null
+        val loadMoreState = MutableLiveData<LoadMoreState>()
+        private var query: String? = null
+        private var _hasMore: Boolean = false
+        val hasMore
+            get() = _hasMore
+
+        init {
+            reset()
+        }
+
+        fun queryNextPage(query: String){
+            if(this.query == query){
+                return
+            }
+            unregister()
+            this.query = query
+            nextPageLiveData = repository.searchNextPage(query)
+            loadMoreState.value = LoadMoreState(
+                isRunning = true,
+                errorMessage = null
+            )
+            nextPageLiveData?.observeForever(this)
+        }
+
+        override fun onChanged(result: Resource<Boolean>?) {
+            if(result == null){
+                reset()
+            }else{
+                when(result.status){
+                    Status.SUCCESS ->{
+                        _hasMore = result.data == true
+                        unregister()
+                        loadMoreState.setValue(
+                            LoadMoreState(
+                                isRunning = false,
+                                errorMessage = null
+                            )
+                        )
+                    }
+                    Status.ERROR -> {
+                        _hasMore = true
+                        unregister()
+                        loadMoreState.setValue(
+                            LoadMoreState(
+                                isRunning = false,
+                                errorMessage = result.message
+                            )
+                        )
+                    }
+                    Status.LOADING ->{
+
+                    }
+                }
+            }
+        }
+
+        private fun unregister(){
+            nextPageLiveData?.removeObserver(this)
+            nextPageLiveData = null
+            if(_hasMore){
+                query = null
+            }
+        }
+
+        fun reset(){
+            unregister()
+            _hasMore = true
+            loadMoreState.value = LoadMoreState(
+                isRunning = false,
+                errorMessage = null
+            )
         }
     }
 }
