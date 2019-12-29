d3cddfe9c6f320b8bd257a517a1ce75febdce694
Alberto <info@androiddesdecero.com>
2019-12-29 12:00:52 +0100

    3

--- app/src/main/java/com/androiddesdecero/mvvmkotlin/ui/search/SearchFragment.kt
+++ app/src/main/java/com/androiddesdecero/mvvmkotlin/ui/search/SearchFragment.kt
@@ -8,7 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingComponent
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.androiddesdecero.mvvmkotlin.AppExecutors
 
 import com.androiddesdecero.mvvmkotlin.R
@@ -16,6 +18,7 @@ import com.androiddesdecero.mvvmkotlin.binding.FragmentDataBindingComponent
 import com.androiddesdecero.mvvmkotlin.databinding.FragmentSearchBinding
 import com.androiddesdecero.mvvmkotlin.di.Injectable
 import com.androiddesdecero.mvvmkotlin.ui.common.RepoListAdapter
+import com.androiddesdecero.mvvmkotlin.ui.common.RetryCallback
 import com.androiddesdecero.mvvmkotlin.utils.autoCleared
 import com.androiddesdecero.mvvmkotlin.viewmodel.GithubViewModelFactory
 import javax.inject.Inject
@@ -34,6 +37,10 @@ class SearchFragment : Fragment(), Injectable {
 
     var adapter by autoCleared<RepoListAdapter>()
 
+    val searchViewModel: SearchViewModel by viewModels{
+        viewModelFactory
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -47,6 +54,32 @@ class SearchFragment : Fragment(), Injectable {
             dataBindingComponent)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.setLifecycleOwner (viewLifecycleOwner)
+        initRecyclerView()
+        val rvAdapter = RepoListAdapter(
+            dataBindingComponent = dataBindingComponent,
+            appExecutors = appExecutors,
+            showFullName = true
+        ){
+            repo->
+            findNavController().navigate(
+                SearchFragmentDirections.actionSearchFragmentToRepoFragment(repo.name, repo.owner.login)
+            )
+        }
+        binding.query = searchViewModel.query
+        binding.repoList.adapter = rvAdapter
+        adapter = rvAdapter
+
+        initSearchInputListener()
+
+        binding.callback = object: RetryCallback{
+            override fun retry() {
+                searchViewModel.refresh()
+            }
+        }
     }
 
 
